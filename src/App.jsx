f2b97b7afa634eddac2cdc92747212d557ec86a7
Heihachi <heiariim1@gmail.com>
2025-05-30 01:06:09 -1000

    ✨ F02: APP>Add routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ErrorPage from "./components/pages/error404/ErrorPage";
 import LoginPage from "./components/pages/login/LoginPage";
+import OrderPage from "./components/pages/order/OrderPage";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/order" element={<OrderPage />} />
+
+        {/* Route catch-all pour 404 */}
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/pages/error404/ErrorPage.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <>
+      <div>Sorry this page does not exist</div>
+      <button>Retour a l'acceuil</button>
+    </>
+  );
+}
+
+export default NotFound;
