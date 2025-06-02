d0962d755532a45567eb8a0567b841f3f6df1c10
Heihachi <heiariim1@gmail.com>
2025-06-01 17:01:27 -1000

    🚧 F03: Work in progress

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,7 +1,23 @@
+import styled from "styled-components";
 import LoginForm from "./LoginForm";
+import bgImage from "../../../assets/burger-background.jpg";
 
 function LoginPage() {
-  return <LoginForm />;
+  return (
+    <LoginPageStyled>
+      <LoginForm />
+    </LoginPageStyled>
+  );
 }
 
 export default LoginPage;
+
+const LoginPageStyled = styled.div`
+  background-color: red;
+  //background-image: url(${bgImage});
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100dvh;
+`;
