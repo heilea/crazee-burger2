04c954509d084ddda350d1a2c73bc487637c4e5b
Heihachi <heiariim1@gmail.com>
2025-06-03 13:46:36 -1000

    💄F03: LOGIN> LoginForm > Add styles

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
-
+import { theme } from "../../../theme/index";
+import { IoPersonCircle } from "react-icons/io5";
 function LoginForm() {
   const [userName, setUserName] = useState("");
   const navigate = useNavigate();
@@ -18,17 +19,19 @@ function LoginForm() {
   };
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenus chez nous !</h1>
-      <br />
-      <h2>Connectez-vous</h2>
-      <input
-        type="text"
-        value={userName}
-        placeholder="Entrez votre prénom"
-        onChange={handleChange}
-        required
-      />
-      <button>Accéder à mon espace</button>
+      <h1 className="title">Bienvenus chez nous !</h1>
+      <hr />
+      <h2 className="subtitle">Connectez-vous</h2>
+      <div className="input-wrap">
+        <input
+          type="text"
+          value={userName}
+          placeholder="Entrez votre prénom"
+          onChange={handleChange}
+          required
+        />
+        <button>Accéder à mon espace</button>
+      </div>
     </LoginFormStyled>
   );
 }
@@ -36,9 +39,54 @@ function LoginForm() {
 export default LoginForm;
 
 const LoginFormStyled = styled.form`
-  border: 5px solid green;
+  //background-color: green;
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0px auto;
+  padding: 2.5rem 2rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
 
+  hr {
+    border: 1.5px solid ${theme.colors.primary};
+    margin-top: 32px;
+    margin-bottom: 40px;
+  }
   h1 {
-    border: 2px solid blue;
+    color: ${theme.colors.white};
+    font-size: ${theme.fonts.P5};
+  }
+
+  h2 {
+    color: ${theme.colors.white};
+    font-size: ${theme.fonts.P4};
+    margin: 20px 10px 10px;
+  }
+
+  .input-wrap {
+    display: flex;
+    flex-direction: column;
+    gap: 18px;
+    input {
+      font-family: "Open Sans", sans-serif;
+
+      color: #d3d3d3;
+      background-color: ${theme.colors.background_white};
+      font-size: ${theme.fonts.P0};
+      border-radius: ${theme.borderRadius.round};
+      font-weight: ${theme.weights.regular};
+      padding: 18px 24px;
+    }
+    button {
+      font-family: "Open Sans", sans-serif;
+
+      background-color: ${theme.colors.primary};
+      color: ${theme.colors.white};
+      font-size: ${theme.fonts.P0};
+      border-radius: ${theme.borderRadius.round};
+      font-weight: ${theme.weights.bold};
+      padding: 18px 24px;
+    }
   }
 `;
