6ac28949d4a29eeaac099f0a4e8df9b39cb8bcf6
Heihachi <heiariim1@gmail.com>
2025-06-05 14:31:23 -1000

    👷 F04 : ORDER > OrderPage > Finished layout

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,7 +1,7 @@
-import { Link, useParams } from "react-router";
+import { useParams } from "react-router";
 import styled from "styled-components";
-import RawLogo from "../../reusable-ui/Logo";
-import { IoPersonCircle } from "react-icons/io5";
+import NavBar from "./NavBar";
+import Main from "./Main";
 
 function OrderPage() {
   const { userName } = useParams();
@@ -9,20 +9,9 @@ function OrderPage() {
   return (
     <OrderPageStyled>
       <div className="container">
-        <nav>
-          {<SmallLogo />}
-          <div className="profile">
-            <div className="wrap">
-              <p>Hey, {userName}</p>
-              <Link to={"/"}>
-                <button>Deconnexion</button>
-              </Link>
-            </div>
-            {<IoPersonCircle className="icon" />}
-          </div>
-        </nav>
+        <NavBar username={userName} />
+        <Main />
       </div>
-      <div className="main"></div>
     </OrderPageStyled>
   );
 }
@@ -34,42 +23,13 @@ const OrderPageStyled = styled.div`
   height: 100vh;
   display: flex;
   justify-content: center;
+  align-items: center;
 
   .container {
     background: purple;
-    width: 1400px;
-
-    nav {
-      background-color: blue;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-
-      .logo {
-        color: pink;
-        display: block;
-      }
-
-      .profile {
-        background: red;
-        display: flex;
-        align-items: center;
-
-        .wrap {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-        }
-
-        .icon {
-          color: #747b91;
-        }
-      }
-    }
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 90vh;
   }
-`;
-
-const SmallLogo = styled(RawLogo)`
-  transform: scale(0.4); //Trying to size down Logo by inversing it
-  width: 200px;
 `;
